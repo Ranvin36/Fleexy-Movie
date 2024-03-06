4457fb11c3482ec0a60eab9fe628dba8a8d80a59
Ranvin36 <141607057+Ranvin36@users.noreply.github.com>
2024-03-06 13:57:03 +0530

    Cast Names

--- src/Components/cast.js
+++ src/Components/cast.js
@@ -3,12 +3,13 @@ import { isContentEditable } from "@testing-library/user-event/dist/utils"
 function Cast({item}){
     const CastNames = String(item.name)
     const CastNamesSplit=(CastNames.split(" "))
+    const CastName = item.name.length > 12 ? item.name.slice(0,12) + ".." : item.name
 return(
     <div className='castDetails'>
         {item.profile_path &&
         <>
          <img src ={`https://image.tmdb.org/t/p/original${item.profile_path}`} className='castImg'/>
-          <p>{item.name.slice(0,12)}</p>
+          <p>{CastName}</p>
         </>}
     </div>
 )
